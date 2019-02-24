32250ef2e99c2707f11945c0e9145e37ec05a554
Sean Raborg <sean.raborg@gmail.com>
2019-02-24 04:49:04 -0800

    Refactored DFS

--- src/SearchStrategy.py
+++ src/SearchStrategy.py
@@ -11,11 +11,42 @@ class SearchStrategy(abc.ABC):
 class DepthFirstSearch(SearchStrategy):
 
     def search(self, board_, *args, **kargs):
-        possible_solutions = []
-        move_sequence = []
-        max_iterations_ = kargs.get("max_iterations")
-        self._search(board_, move_sequence, possible_solutions, 0, max_iterations=max_iterations_)
-        return possible_solutions
+
+        solutions = []
+
+        # DFS Structures
+        stack = []
+        stack.append((board_, [], 0))
+
+        while stack:
+            board, move_sequence_, depth = stack.pop()
+            pieces = board.num_of_pieces()
+
+            # Check For Win Condition
+            # Possible Solution Found
+            if pieces < 2 and depth:
+                solutions.append(move_sequence_)
+
+                # Make Sure "max_iterations" Condition is met
+                if depth is kargs.get("max_iterations"):
+                    break
+
+            # Queue Up Remaining
+            attack_positions = board.legal_attack_positions()
+            for position in attack_positions:  # Not Empty
+                attacker = board.position(position)
+                attacks = board.valid_attacks(position)
+
+                possible_attacks = []
+                for target_position in attacks:
+                    move_sequence = move_sequence_.copy()
+                    next_board = copy.deepcopy(board)
+                    next_board.capture(position, target_position)
+                    move = (attacker, position, target_position)
+                    move_sequence.append(move)
+
+                    stack.append((next_board, move_sequence, depth + 1))
+        return solutions
 
     def _search(self, board_, move_sequence_, possible_solutions_, depth, **kargs):
 
